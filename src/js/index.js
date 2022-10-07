9abfbd1c254bf28273f6f82167a6b434fd7169e1
Javier flores <javier.flores@ivitech.com.mx>
2022-10-07 18:53:28 -0500

    v0.35.7 productos 70% terminado

--- src/js/index.js
+++ src/js/index.js
@@ -5,8 +5,234 @@ $('.carousel').carousel({
 
 
 
-function myFunction() {
+function seleccionar_1() {
+    var cardActive1 = document.getElementById("card-1");
+    cardActive1.classList.add("active_1")
+    var cardNotActive1 = document.getElementById("card-1");
+    cardNotActive1.classList.remove("not-active_1")
 
-    console.log("ola")
+    var cardActive2 = document.getElementById("card-2");
+    cardActive2.classList.remove("active_2")
+    var cardActive3 = document.getElementById("card-3");
+    cardActive3.classList.remove("active_3")
+    var cardActive4 = document.getElementById("card-4");
+    cardActive4.classList.remove("active_4")
+    var cardActive5 = document.getElementById("card-5");
+    cardActive5.classList.remove("active_5")
+    var cardActive6 = document.getElementById("card-6");
+    cardActive6.classList.remove("active_6")
+    var cardActive7 = document.getElementById("card-7");
+    cardActive7.classList.remove("active_7")
+
+    var cardNotActive2 = document.getElementById("card-2");
+    cardNotActive2.classList.add("not-active_2")
+    var cardNotActive3 = document.getElementById("card-3");
+    cardNotActive3.classList.add("not-active_3")
+    var cardNotActive4 = document.getElementById("card-4");
+    cardNotActive4.classList.add("not-active_4")
+    var cardNotActive5 = document.getElementById("card-5");
+    cardNotActive5.classList.add("not-active_5")
+    var cardNotActive6 = document.getElementById("card-6");
+    cardNotActive6.classList.add("not-active_6")
+    var cardNotActive7 = document.getElementById("card-7");
+    cardNotActive7.classList.add("not-active_7")
+
+}
+function seleccionar_2() {
+    var cardActive2 = document.getElementById("card-2");
+    cardActive2.classList.add("active_2")
+    var cardNotActive2 = document.getElementById("card-2");
+    cardNotActive2.classList.remove("not-active_2")
+
+    var cardActive1 = document.getElementById("card-1");
+    cardActive1.classList.remove("active_1")
+    var cardActive3 = document.getElementById("card-3");
+    cardActive3.classList.remove("active_3")
+    var cardActive4 = document.getElementById("card-4");
+    cardActive4.classList.remove("active_4")
+    var cardActive5 = document.getElementById("card-5");
+    cardActive5.classList.remove("active_5")
+    var cardActive6 = document.getElementById("card-6");
+    cardActive6.classList.remove("active_6")
+    var cardActive7 = document.getElementById("card-7");
+    cardActive7.classList.remove("active_7")
+
+    var cardNotActive1 = document.getElementById("card-1");
+    cardNotActive1.classList.add("not-active_1")
+    var cardNotActive3 = document.getElementById("card-3");
+    cardNotActive3.classList.add("not-active_3")
+    var cardNotActive4 = document.getElementById("card-4");
+    cardNotActive4.classList.add("not-active_4")
+    var cardNotActive5 = document.getElementById("card-5");
+    cardNotActive5.classList.add("not-active_5")
+    var cardNotActive6 = document.getElementById("card-6");
+    cardNotActive6.classList.add("not-active_6")
+    var cardNotActive7 = document.getElementById("card-7");
+    cardNotActive7.classList.add("not-active_7")
+
+}
+function seleccionar_3() {
+    var cardActive3 = document.getElementById("card-3");
+    cardActive3.classList.add("active_3")
+    var cardNotActive3 = document.getElementById("card-3");
+    cardNotActive3.classList.remove("not-active_3")
+
+    var cardActive1 = document.getElementById("card-1");
+    cardActive1.classList.remove("active_1")
+    var cardActive2 = document.getElementById("card-2");
+    cardActive2.classList.remove("active_2")
+    var cardActive4 = document.getElementById("card-4");
+    cardActive4.classList.remove("active_4")
+    var cardActive5 = document.getElementById("card-5");
+    cardActive5.classList.remove("active_5")
+    var cardActive6 = document.getElementById("card-6");
+    cardActive6.classList.remove("active_6")
+    var cardActive7 = document.getElementById("card-7");
+    cardActive7.classList.remove("active_7")
+
+    var cardNotActive1 = document.getElementById("card-1");
+    cardNotActive1.classList.add("not-active_1")
+    var cardNotActive2 = document.getElementById("card-2");
+    cardNotActive2.classList.add("not-active_2")
+    var cardNotActive4 = document.getElementById("card-4");
+    cardNotActive4.classList.add("not-active_4")
+    var cardNotActive5 = document.getElementById("card-5");
+    cardNotActive5.classList.add("not-active_5")
+    var cardNotActive6 = document.getElementById("card-6");
+    cardNotActive6.classList.add("not-active_6")
+    var cardNotActive7 = document.getElementById("card-7");
+    cardNotActive7.classList.add("not-active_7")
+
+}
+function seleccionar_4() {
+    var cardActive4 = document.getElementById("card-4");
+    cardActive4.classList.add("active_4")
+    var cardNotActive4 = document.getElementById("card-4");
+    cardNotActive4.classList.remove("not-active_4")
+
+    var cardActive1 = document.getElementById("card-1");
+    cardActive1.classList.remove("active_1")
+    var cardActive2 = document.getElementById("card-2");
+    cardActive2.classList.remove("active_2")
+    var cardActive3 = document.getElementById("card-3");
+    cardActive3.classList.remove("active_3")
+    var cardActive5 = document.getElementById("card-5");
+    cardActive5.classList.remove("active_5")
+    var cardActive6 = document.getElementById("card-6");
+    cardActive6.classList.remove("active_6")
+    var cardActive7 = document.getElementById("card-7");
+    cardActive7.classList.remove("active_7")
+
+    var cardNotActive1 = document.getElementById("card-1");
+    cardNotActive1.classList.add("not-active_1")
+    var cardNotActive2 = document.getElementById("card-2");
+    cardNotActive2.classList.add("not-active_2")
+    var cardNotActive3 = document.getElementById("card-3");
+    cardNotActive3.classList.add("not-active_3")
+    var cardNotActive5 = document.getElementById("card-5");
+    cardNotActive5.classList.add("not-active_5")
+    var cardNotActive6 = document.getElementById("card-6");
+    cardNotActive6.classList.add("not-active_6")
+    var cardNotActive7 = document.getElementById("card-7");
+    cardNotActive7.classList.add("not-active_7")
+
+}
+function seleccionar_5() {
+    var cardActive5 = document.getElementById("card-5");
+    cardActive5.classList.add("active_5")
+    var cardNotActive5 = document.getElementById("card-5");
+    cardNotActive5.classList.remove("not-active_5")
+
+    var cardActive1 = document.getElementById("card-1");
+    cardActive1.classList.remove("active_1")
+    var cardActive2 = document.getElementById("card-2");
+    cardActive2.classList.remove("active_2")
+    var cardActive3 = document.getElementById("card-3");
+    cardActive3.classList.remove("active_3")
+    var cardActive4 = document.getElementById("card-4");
+    cardActive4.classList.remove("active_4")
+    var cardActive6 = document.getElementById("card-6");
+    cardActive6.classList.remove("active_6")
+    var cardActive7 = document.getElementById("card-7");
+    cardActive7.classList.remove("active_7")
+
+    var cardNotActive1 = document.getElementById("card-1");
+    cardNotActive1.classList.add("not-active_1")
+    var cardNotActive2 = document.getElementById("card-2");
+    cardNotActive2.classList.add("not-active_2")
+    var cardNotActive3 = document.getElementById("card-3");
+    cardNotActive3.classList.add("not-active_3")
+    var cardNotActive4 = document.getElementById("card-4");
+    cardNotActive4.classList.add("not-active_4")
+    var cardNotActive6 = document.getElementById("card-6");
+    cardNotActive6.classList.add("not-active_6")
+    var cardNotActive7 = document.getElementById("card-7");
+    cardNotActive7.classList.add("not-active_7")
+
+}
+function seleccionar_6() {
+    var cardActive6 = document.getElementById("card-6");
+    cardActive6.classList.add("active_6")
+    var cardNotActive6 = document.getElementById("card-6");
+    cardNotActive6.classList.remove("not-active_6")
+
+    var cardActive1 = document.getElementById("card-1");
+    cardActive1.classList.remove("active_1")
+    var cardActive2 = document.getElementById("card-2");
+    cardActive2.classList.remove("active_2")
+    var cardActive3 = document.getElementById("card-3");
+    cardActive3.classList.remove("active_3")
+    var cardActive4 = document.getElementById("card-4");
+    cardActive4.classList.remove("active_4")
+    var cardActive5 = document.getElementById("card-5");
+    cardActive5.classList.remove("active_5")
+    var cardActive7 = document.getElementById("card-7");
+    cardActive7.classList.remove("active_7")
+
+    var cardNotActive1 = document.getElementById("card-1");
+    cardNotActive1.classList.add("not-active_1")
+    var cardNotActive2 = document.getElementById("card-2");
+    cardNotActive2.classList.add("not-active_2")
+    var cardNotActive3 = document.getElementById("card-3");
+    cardNotActive3.classList.add("not-active_3")
+    var cardNotActive4 = document.getElementById("card-4");
+    cardNotActive4.classList.add("not-active_4")
+    var cardNotActive5 = document.getElementById("card-5");
+    cardNotActive5.classList.add("not-active_5")
+    var cardNotActive7 = document.getElementById("card-7");
+    cardNotActive7.classList.add("not-active_7")
+
+}
+function seleccionar_7() {
+    var cardActive7 = document.getElementById("card-7");
+    cardActive7.classList.add("active_7")
+    var cardNotActive7 = document.getElementById("card-7");
+    cardNotActive7.classList.remove("not-active_7")
+
+    var cardActive1 = document.getElementById("card-1");
+    cardActive1.classList.remove("active_1")
+    var cardActive2 = document.getElementById("card-2");
+    cardActive2.classList.remove("active_2")
+    var cardActive3 = document.getElementById("card-3");
+    cardActive3.classList.remove("active_3")
+    var cardActive4 = document.getElementById("card-4");
+    cardActive4.classList.remove("active_4")
+    var cardActive5 = document.getElementById("card-5");
+    cardActive5.classList.remove("active_5")
+    var cardActive6 = document.getElementById("card-6");
+    cardActive6.classList.remove("active_6")
+
+    var cardNotActive1 = document.getElementById("card-1");
+    cardNotActive1.classList.add("not-active_1")
+    var cardNotActive2 = document.getElementById("card-2");
+    cardNotActive2.classList.add("not-active_2")
+    var cardNotActive3 = document.getElementById("card-3");
+    cardNotActive3.classList.add("not-active_3")
+    var cardNotActive4 = document.getElementById("card-4");
+    cardNotActive4.classList.add("not-active_4")
+    var cardNotActive5 = document.getElementById("card-5");
+    cardNotActive5.classList.add("not-active_5")
+    var cardNotActive6 = document.getElementById("card-6");
+    cardNotActive6.classList.add("not-active_6")
 
 }
