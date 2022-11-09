5dc52f8851428fc117d352b014320f8081a0be1b
Javier flores <javier.flores@ivitech.com.mx>
2022-11-09 14:54:29 -0600

    se integra correcion de scroll

--- js/Carousel.js
+++ js/Carousel.js
@@ -75,3 +75,14 @@
   onOrientationChange();
 
 
+  $(".carousel").swipe({
+
+    swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
+  
+      if (direction == 'left') $(this).carousel('next');
+      if (direction == 'right') $(this).carousel('prev');
+  
+    },
+    allowPageScroll:"vertical"
+  
+  });
